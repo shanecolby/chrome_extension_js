0a5c4d023aaabb1d1d713c40ee84cc65f28130ee
shanecolby <shanecolbyzero8@gmail.com>
2021-07-04 10:49:52 -0500

    create listItems and have it loop through each one for leads length

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ inputBtn.addEventListener("click", function () {
   console.log(myLeads)
 })
 
+let listItems = ""
+
 for (let i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+  listItems += "<li>" + myLeads[i] + "</li>"
+  console.log(listItems)
   // const li = document.createElement("li")
   // li.textContent = myLeads[i]
   // ulEl.append(li)
 }
+ulEl.innerHTML = listItems
+
